9ec376560fddc9c15404bb32cc8d3ac636a206e1
MERRYS MACBOOK <merrysmacbook@MERRYSs-MBP.attlocal.net>
2023-09-28 18:17:58 -0500

    updated my schema and seeds sql file

--- seeds.sql
+++ seeds.sql
@@ -1,19 +1,18 @@
-INSERT INTO department(id, name)
+INSERT INTO department(name)
 VALUES('HR'),
     ('Engineering'),
     ('Marketing');
 
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO role ( title, salary, department_id)
 VALUES('HR Manager', 75000, 1),
     ('Software Engineer', 80000, 2),
     ('Marketing Specialist', 60000, 3);
     
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+INSERT INTO employee ( first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, NULL),
     ('Jane', 'Smith', 2, 1),
     ('Alice', 'Johnson', 3, 1),
     ('Bob', 'Brown', 1, NULL);
 
-COMMIT;
