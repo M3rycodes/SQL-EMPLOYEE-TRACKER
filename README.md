5c2bdcda6558bab8e525cfe3e4d97945cad3df7d
MERRYS MACBOOK <merrysmacbook@MERRYSs-MBP.attlocal.net>
2023-10-02 20:37:26 -0500

    adding a video

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SQL-EMPLOYEE-TRACKER
 To be able to select, update, or delete a database
+
+
--- schema.sql
+++ schema.sql
@@ -20,10 +20,12 @@ CREATE TABLE employee (
   id INT AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
 
 
